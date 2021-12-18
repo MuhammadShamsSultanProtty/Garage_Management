cc4bcaa09584c64967a3c21934e01e454fa30907
MuhammadShamsSultanProtty <shamssultanprottoy@gmail.com>
2021-12-18 19:30:43 +0600

    adminrole735done

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,12 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+    const handleOnBlur = e => {
+        setEmail(e.target.value);
+    }
+    const handleAdminSubmit = e => {
+        const user = { email };
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+        e.preventdefault();
+    }
     return (
         <div>
             <h2>Make admin</h2>
-            {/* <form onSubmit={}> */}
-
-            {/* </form> */}
+            <form onSubmit={handleAdminSubmit}>
+                <TextField
+                    label="Email"
+                    type="email"
+                    onBlur={handleOnBlur}
+                    variant="standard" />
+                <Button type='submit' variant="contained" color="success">
+                    Make Admin
+                </Button>
+            </form>
         </div >
     );
 };
